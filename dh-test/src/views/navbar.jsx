814bc7b4df3dc6b1dd5115f3622bfca1e74f164b
Quroz <81308705+Quroz@users.noreply.github.com>
2021-12-01 11:46:18 +0100

    update

--- dh-test/src/views/navbar.jsx
+++ dh-test/src/views/navbar.jsx
@@ -10,14 +10,18 @@ function Navbar(props) {
                 <Link to = "/" style={{ textDecoration: 'none', color:"#1a67ab" }}><h1>Find The Duck</h1></Link>
 
                 <ul>
-                {props.user?(<div>
+                {props.user?(<div className = "test">   {/* FIXA !!!!!!!  */}
+                     {props.user.email}
+                    <li> <Link to = "/register" style={{ textDecoration: 'none',  color:"#1a67ab" }}>Register</Link> </li>
                     <button onClick = {props.logout}>Sign out</button>
-                    {props.user.email}
                     </div>)
                 : (
+                    <div className ="test">
                     <li> <Link to = "/login" style={{ textDecoration: 'none',  color:"#1a67ab" }}>Login</Link> </li>
+                    <li> <Link to = "/register" style={{ textDecoration: 'none',  color:"#1a67ab" }}>Register</Link> </li>
+                    </div>
                  )}
-                <li> <Link to = "/register" style={{ textDecoration: 'none',  color:"#1a67ab" }}>Register</Link> </li>
+             
                 </ul>
             </nav>
         </div>
